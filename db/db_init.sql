7e4c7d578f198442e6804902788bb92549798903
Geoff Lorne <geo.lorne@gmail.com>
2018-03-10 12:35:08 -0800

    db init

---
+++ db/db_init.sql
@@ -0,0 +1,43 @@
+DO
+$body$
+BEGIN
+  IF NOT EXISTS (
+      SELECT                       -- SELECT list can stay empty for this
+      FROM   pg_catalog.pg_user
+      WHERE  usename = 'navi') THEN
+
+    CREATE ROLE navi LOGIN PASSWORD 'heylisten';
+  END IF;
+END
+$body$;
+
+ALTER USER navi WITH SUPERUSER;
+GRANT ALL PRIVILEGES ON DATABASE clarifly TO navi;
+
+\connect clarifly
+
+
+CREATE TABLE IF NOT EXISTS rooms (
+  room_code   VARCHAR(4) PRIMARY KEY,
+  creator_id  INT,
+  start_time  TIMESTAMP,
+  end_time    TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  u_id        SERIAL PRIMARY KEY,
+  ip_addr     VARCHAR(15),
+  room_code   VARCHAR(4) REFERENCES rooms(room_code),
+  join_time   TIMESTAMP,
+  leave_time  TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+  q_id        SERIAL PRIMARY KEY,
+  u_id        INT REFERENCES users(u_id),
+  room_code   VARCHAR(4) REFERENCES rooms(room_code),
+  text        VARCHAR(140),
+  votes       INT,
+  reports     INT,
+  ask_time    TIMESTAMP
+);
