a4c69fa9113f841103fe81c94d1ed3a8c853ba6f
hcape <hcape@uvic.ca>
2018-03-10 17:00:33 -0800

    question mage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/LandingPage'
+import QuestionPage from '@/components/QuestionPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LandingPage',
       component: LandingPage
+    },
+    {
+      path: '/test',
+      name: 'QuestionPage',
+      component: QuestionPage
     }
   ]
 })
